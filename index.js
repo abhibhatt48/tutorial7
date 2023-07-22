c864ae39958526ec5940235236e0a7bda28d8de8
Abhishek Bhatt <47849440+abhibhatt48@users.noreply.github.com>
2023-07-22 13:32:35 -0300

    Added tutorial 7.

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const itemsRouter = require('./routes/items');
+app.use('/items', itemsRouter);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
